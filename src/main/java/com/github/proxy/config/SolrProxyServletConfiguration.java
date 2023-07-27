d733389240818cb4e3899ea759df55bece8d39d6
lxhcaicai <2778763221@qq.com>
2023-07-27 22:46:24 +0800

    add http-proxy test 通过 8082端口反向代理百度网址

---
+++ src/main/java/com/github/proxy/config/SolrProxyServletConfiguration.java
@@ -0,0 +1,37 @@
+package com.github.proxy.config;
+
+import org.mitre.dsmiley.httpproxy.ProxyServlet;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySources;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Properties;
+
+@Configuration
+public class SolrProxyServletConfiguration implements EnvironmentAware {
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        Properties properties= (Properties) bindResult.get();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new ProxyServlet(), properties.getProperty("servlet_url"));
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_TARGET_URI, properties.getProperty("target_url"));
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, properties.getProperty("logging_enabled", "false"));
+        return servletRegistrationBean;
+    }
+
+    private BindResult bindResult;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        Iterable sources = ConfigurationPropertySources.get(environment);
+        Binder binder = new Binder(sources);
+        BindResult bindResult = binder.bind("proxy.solr", Properties.class);
+        this.bindResult = bindResult;
+    }
+
+}
